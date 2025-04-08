975b35352536bbc8cc458a75e6dd050b598253b6
Franco Narvaez <juanfranconarvaez@gmail.com>
2025-04-07 22:22:12 -0300

    Pelicula interface

---
+++ src/interfaces/Pelicula.ts
@@ -0,0 +1,9 @@
+export interface Pelicula {
+    id: number;
+    titulo: string;
+    anio: number;
+    generos: Array<string>;
+    director: string;
+    portada: string;
+    likes: number;
+  }
