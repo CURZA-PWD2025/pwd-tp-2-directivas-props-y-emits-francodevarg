806130b31661c2b2dbcb659438bf69f187c16cf5
Franco Narvaez <juanfranconarvaez@gmail.com>
2025-04-07 22:26:13 -0300

    Listado de Pelicula y CardComponent para los detalles

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,17 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1>Lista de Películas</h1>
+  <CardComponent
+    v-for="movie in movies"
+    :key="movie.id"
+    :movie="movie"
+  />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script lang="ts" setup>
+import { ref } from 'vue'
+import peliculas  from '../resource/movies'
+import type { Pelicula } from './interfaces/Pelicula'
+import CardComponent from './components/CardComponent.vue'
+
+const movies = ref<Pelicula[]>(peliculas)
+</script>
---
+++ src/components/CardComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="card">
+    <h2>{{ movie.titulo }} ({{ movie.anio }})</h2>
+    <p><strong>Géneros: </strong>
+        <span v-for="(genero, index) in movie.generos" :key="index">
+          {{ genero }}<span v-if="index < movie.generos.length - 1">, </span>
+        </span>
+    </p>
+    <p><strong>Director:</strong> {{ movie.director }}</p>
+    
+    <img :src="movie.portada" :alt="movie.titulo">
+    
+  </div>
+</template>
+
+<script lang="ts" setup>
+import type { Pelicula } from '../interfaces/Pelicula'
+
+const props = defineProps<{ movie: Pelicula }>()
+
+</script>
